1e90df12aa8100a67d6b5da6bd568572882f806e
Jeremy Bergen <jbergen@coloradomesa.edu>
2025-01-29 09:50:41 -0700

    wednesday 0129

---
+++ datatypes.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1;
+    int side1;
+    int side2;
+    float radius;
+    unsigned int uint1;
+    long long1;
+
+    char thisIsCharCharBinks;
+    char thisIsTheMostAwesomeCharThatHasEverCharredInTheHistoryOfCharring;
+    float r;
+
+    num1 = 42;
+
+    cout << num1 << endl;
+
+    return 0;
+}
