b6325e004480c466f314712d2fba5d57c7c28e3a
Jeremy Bergen <jbergen@coloradomesa.edu>
2025-01-30 09:50:50 -0700

    thursday 0130

--- datatypes.cpp
+++ datatypes.cpp
@@ -1,23 +1,52 @@
+/*
+Name: Jeremy Bergen
+Date: 01/30/25
+
+This is for different datatypes and to demonstrate them
+*/
+
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    int num1;
-    int side1;
-    int side2;
-    float radius;
-    unsigned int uint1;
-    long long1;
+    string num1; // At the end of the line
 
-    char thisIsCharCharBinks;
-    char thisIsTheMostAwesomeCharThatHasEverCharredInTheHistoryOfCharring;
-    float r;
+    // string num2;
+    // string sum;
 
-    num1 = 42;
+    // cout << "Please enter two numbers separated by a space: ";
+    // cin >> num1 >> num2;
+    // // cout << "Please enter another number: ";
+    // // cin >> num2;
 
-    cout << num1 << endl;
+    // sum = num1 + num2;
+
+    // cout << num1 << " + " << num2 << " = " 
+    //      << sum << endl;
+
+    // cout << "sizeof(num1): " 
+    //      << sizeof(num1) << endl;
+
+    // long num1;
+    // long side1;
+    // long side2;
+    // float radius;
+    // unsigned int uint1;
+    // long long1;
+
+    // char thisIsCharCharBinks;
+    // char thisIsTheMostAwesomeCharThatHasEverCharredInTheHistoryOfCharring;
+    // char this_is_a_char;
+    // float r;
+
+    // num1 = 42;
+
+    // cout << "num1: " << num1 << endl;
+    // cout << "&num1:  " << &num1 << endl;
+    // cout << "&side1: " << &side1 << endl;
+    // cout << "&side2: " << &side2 << endl;
 
     return 0;
 }
