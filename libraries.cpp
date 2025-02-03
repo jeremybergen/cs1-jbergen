e8e761c75d25482bc00de9aa5df61d899009e3d7
Jeremy Bergen <jbergen@coloradomesa.edu>
2025-02-03 09:51:43 -0700

    monday 0203

---
+++ libraries.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int num1;
+    int num2;
+
+    cout << "Enter two numbers separated by a space: ";
+    cin >> num1 >> num2;
+
+    // cout << num1 << "^" << num2 << ": " 
+    //      << pow(num1, num2) << endl;
+
+
+    // cout << "abcdefgh\r1" << endl;
+    // string fName;
+    // string lName;
+    // string fullName;
+
+    // cout << "What is your first name: ";
+    // cin >> fName;
+
+    // cout << "What is your last name: ";
+    // cin >> lName;
+
+    // fullName = "Hello " + fName + " " + lName + "\n";
+
+    // cout << fullName;
+
+    // cout << "Hello " 
+    //      << fName << " " << lName 
+    //      << endl;
+
+
+    return 0;
+}
