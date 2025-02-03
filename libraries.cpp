b0818bb20f15685b15a4e8ea529bb5bf2d70dbd1
Jeremy Bergen <jbergen@coloradomesa.edu>
2025-02-03 16:28:31 -0700

    tuesday 0204

--- libraries.cpp
+++ libraries.cpp
@@ -1,16 +1,27 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <iomanip>
+#include <ctime>
+
+#define JEREMYNUM 42
 
 using namespace std;
 
 int main()
 {
-    int num1;
-    int num2;
+    cout << time(0) << endl;
+    // cout << setprecision(10) << left << setfill('=');
+    // cout << setw(13) << "sqrt(2): " << M_SQRT2 << endl;
+    // cout << setw(13) << "sqrt(2): " << sqrt(2) << endl;
+    // cout << setw(13) << "log(2): " << log(2) << endl;
+    // cout << setw(13) << "e: " << M_E << endl;
+    // cout << setw(13) << "jeremynum: " << JEREMYNUM << endl;
+    // int num1;
+    // int num2;
 
-    cout << "Enter two numbers separated by a space: ";
-    cin >> num1 >> num2;
+    // cout << "Enter two numbers separated by a space: ";
+    // cin >> num1 >> num2;
 
     // cout << num1 << "^" << num2 << ": " 
     //      << pow(num1, num2) << endl;
