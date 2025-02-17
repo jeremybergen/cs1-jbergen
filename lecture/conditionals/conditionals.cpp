f5858a1250fedfd17fc00162a6d3d5fd093b0f3f
Jeremy Bergen <jbergen@coloradomesa.edu>
2025-02-17 09:52:08 -0700

    monday 0217

--- lecture/conditionals/conditionals.cpp
+++ lecture/conditionals/conditionals.cpp
@@ -1,17 +1,83 @@
 #include <iostream>
+#include <cmath>
+#include <cctype>
 
 using namespace std;
 
 int main()
 {
-    int num1;
+    int netWorth;
+    int age;
+    char wonLottery = 'N';
     // int num2;
 
-    cout << "Enter a number: ";
+    cout << "Enter your net worth: ";
     // cout << 42/5.0 << endl;
-    cin >> num1;
+    cin >> netWorth;
+    cout << "What is your age: ";
+    cin >> age;
 
-    // if (num%2 )
+    cout << "Did you win the lottery [y/N]: ";
+    cin >> wonLottery;
+
+    // if(netWorth > 2000000 && age > 5 || (wonLottery == 'Y' || wonLottery == 'y'))
+    if((netWorth > 2000000 && age > 5) || tolower(wonLottery) == 'y')
+    {
+        cout << "Congrats, you can retire to a beach" << endl;
+    }
+    else if (netWorth > 1000000 && age > 5)
+    {
+        cout << "Congrats you can retire to Hannah Wyoming......................" << endl;
+    }
+    else if (netWorth > 5000000 && age > 70)
+    {
+        cout << "You should already be retired" << endl;
+    }
+    else
+    {
+        cout << "Keep working" << endl;
+    }
+
+
+    // cout << boolalpha << (num1 < 0) << " " << (num1%2 == 1) << endl;
+    // cout << num1%2 << endl;
+    // if(num1 > 0 && num1%2 == 0)
+    // {
+    //     cout << num1 << " is positive and even" << endl;
+    // }
+    // else if (num1 < 0 && num1%2 == 0)
+    // {
+    //     cout << num1 << " is negative and even" << endl;
+    // }
+    // else if (num1 > 0 && num1%2 == 1)
+    // {
+    //     cout << num1 << " is positive and odd" << endl;
+    // }
+    // else if (num1 < 0 && abs(num1%2) == 1)
+    // {
+    //     cout << num1 << " is negative and odd" << endl;
+    // }
+    // else
+    // {
+    //     cout << num1 << " is zero" << endl;
+    // }
+
+    // if(num1 == 0)
+    // {
+    //     cout << num1 << " is zero" << endl;
+    // }
+    // else if (num1%2 == 0)
+    // {
+    //     cout << num1 << " is even" << endl;
+    //     if(num1 > 0) cout << num1 << " is positive" << endl;
+    //     else cout << num1 << " is negative" << endl;
+    // }
+    // else
+    // {
+    //     cout << num1 << " is not even" << endl;
+    //     if(num1 > 0) cout << num1 << " is positive" << endl;
+    //     else cout << num1 << " is negative" << endl;
+    // }
 
     // if (num1 > num2)
     // {
