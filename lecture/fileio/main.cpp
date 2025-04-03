17971f75b914f655f5acfb80c59aca462e81ebee
Jeremy Bergen <jbergen@coloradomesa.edu>
2025-04-03 09:51:25 -0600

    thursday 0403

--- lecture/fileio/main.cpp
+++ lecture/fileio/main.cpp
@@ -2,17 +2,27 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <cctype>
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
     // int num1, num2, num3, num4, num5;
-    int nums[10];
+    int numLines = 0;
+    
+    
     // vector<int> numbers;
     ifstream fin;
     // fin.open("/home/jbergen/projects/cs1-jbergen/lecture/fileio/files/input.txt");
-    fin.open("files/input.txt");
+    fin.open("files/input.txt", ios_base::binary | ios_base::in);
+
+    if(!fin.is_open())
+    {
+        cout << "Failed to open file" << endl;
+        return 0;
+    }
+
     ofstream fout;
     fout.open("files/output.txt");
 
@@ -20,14 +30,44 @@ int main(int argc, char* argv[])
     string inLine;
     while(getline(fin, inLine))
     {
-        cout << "DEBUG: inLine: " << stoi(inLine) << endl;
+        numLines++;
+    }
+    int nums[numLines];
+
+    // must clear flags before seeking
+    // cout << "DEBUG: numLines: " << numLines << endl;
+    // cout << "tellg: " << fin.tellg() << endl;
+    // fin.seekg(0);
+    // cout << "DEBUG: " << fin.fail() << endl;
+    // fin.clear();
+    // cout << "DEBUG: " << fin.fail() << endl;
+    // cout << "tellg: " << fin.tellg() << endl;
+    // cout << "After seekg" << endl;
+    fin.clear();
+    fin.seekg(0);
+
+    while(getline(fin, inLine))
+    {
+        bool isNumber = true;
+        for(size_t i = 0; i < inLine.size(); i++)
+        {
+            if(inLine.at(0) == '-' && i == 0) continue;
+            if(!isdigit(inLine.at(i))) 
+            {
+                isNumber = false;
+                break;
+            }
+        }
+        if(!isNumber) continue;
+        // cout << "DEBUG: inLine: " << stoi(inLine) << endl;
+        nums[counter] = stoi(inLine);
         counter++;
     }
 
-    // for(int i = 0; i < counter; i++)
-    // {
-    //     cout << "nums[" << i << "]: " << nums[i] << endl;
-    // }
+    for(int i = 0; i < counter; i++)
+    {
+        cout << "nums[" << i << "]: " << nums[i] << endl;
+    }
 
     // for(size_t i = 0; i < numbers.size(); i++)
 
